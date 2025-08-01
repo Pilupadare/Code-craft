b162063a30ba4c8b81166b47d0a66a387c9df047
Pilupadare <kalkidan.ugr-1018-17@aau.edu.et>
2025-08-01 14:02:32 -0700

    JS-exercises.js

--- JS-exercises.js
+++ JS-exercises.js
@@ -1,14 +1,14 @@
 // Part 1: Type Coercion
-console.log('5' + 7);        // '57'
-console.log(Boolean(0));     // false
-console.log('10' - '2');     // 8
-console.log('5' == 5);       // true
-console.log('5' === 5);      // false
-console.log(Boolean(''));    // false
-console.log('5' + true);     // '5true'
-console.log('5' * 2);        // 10
-console.log(0 == false);     // true
-console.log(Boolean(NaN));   // false
+console.log('5' + 7);        
+console.log(Boolean(0));     
+console.log('10' - '2');     
+console.log('5' == 5);       
+console.log('5' === 5);      
+console.log(Boolean(''));    
+console.log('5' + true);     
+console.log('5' * 2);        
+console.log(0 == false);     
+console.log(Boolean(NaN));  
 
 // Part 2: Conditional Checks
 let num = -3;
